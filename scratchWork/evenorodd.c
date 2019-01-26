a3d3616dafe414afe2fb79cf7495bc0555a59cb5
CatLoverKid <aclkeba@gmail.com>
2019-01-26 12:59:39 -0800

    updated scratch work to align columns correctly

--- scratchWork/evenorodd.c
+++ scratchWork/evenorodd.c
@@ -6,7 +6,7 @@ int main()
    char* even = "EVEN";
    char* odd = "ODD";
    for( i = -10; i <= 10; i++ ) {
-      printf( "%i    %i    %s\n", i, i*i, i%2==0? even : odd);
+      printf( "%i\t%i\t%s\n", i, i*i, i%2==0? even : odd);
    }
     return 0;
 }

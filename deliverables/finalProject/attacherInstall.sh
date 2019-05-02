dc6211ae27e829caa05b29523aed8e09be1a4420
Anthony Keba <aclkeba@gmail.com>
2019-05-02 14:26:47 -0700

    removed indev features and testing code

--- deliverables/finalProject/attacherInstall.sh
+++ deliverables/finalProject/attacherInstall.sh
@@ -6,9 +6,6 @@ command -v udevadm > /dev/null
 if [ $? -ne 0 ]; then (>&2 echo "ERROR: Please ensure that udev is used on this Distro"); exit 1; fi
 command -v mount > /dev/null
 if [ $? -ne 0 ]; then (>&2 echo "ERROR: Please ensure that 'mount' is installed"); exit 1; fi
-command -v dialog > /dev/null
-if [ $? -ne 0 ]; then sudo apt-get --assume-yes install dialog; fi
-if [ $? -ne 0 ]; then (>&2 echo "Could not install required packages"); exit 1; fi
 
 # check if directories and files exist
 sudo mkdir -p /etc/udev/rules.d
--- deliverables/finalProject/tmp.sh
+++ deliverables/finalProject/tmp.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 if [ ! $1 ]; then (>&2 echo "ERROR: No Drive found"); exit 1; fi
 if [ ! -b $1 ]; then (>&2 echo "ERROR: $1 is not a drive"); exit 1; fi
-sudo touch /CONFIRMING_THIS_IS_BEING_RUN
-sudo echo "$1" >> /CONFIRMING_THIS_IS_BEING_RUN
 mount | grep $11 > /dev/null 2>&1
 if [ $? != "0" ]; then
 	sudo umount $1
 	sudo mkdir -p /home/catloverkid$1
 	sudo chmod -R 777 /home/catloverkid/dev
-	sudo mount "$1"1 /home/catloverkid$1 2>> /CONFIRMING_THIS_IS_BEING_RUN
+	sudo mount "$1"1 /home/catloverkid$1
 else
-	sudo touch /WE_SHOULDNT_BE_SEEING_THIS
 	(
 	echo o # Create a new empty DOS partition table
 	echo n # Add a new partition
